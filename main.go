0d5df44519e6abca02ccdfc54e50b06ed0fc35b4
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2022-07-21 22:38:38 +0000

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,96 @@
 
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"go.amirul.dev/system/msg"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/mdp/qrterminal"
+	"github.com/probandula/figlet4go"
+	"go.mau.fi/whatsmeow"
+	waProto "go.mau.fi/whatsmeow/binary/proto"
+	"go.mau.fi/whatsmeow/store"
+	"go.mau.fi/whatsmeow/store/sqlstore"
+	"go.mau.fi/whatsmeow/types/events"
+	waLog "go.mau.fi/whatsmeow/util/log"
+	"google.golang.org/protobuf/proto"
+)
+
+func main() {
+	dbLog := waLog.Stdout("Database", "ERROR", true)
+	// Make sure you add appropriate DB connector imports, e.g. github.com/mattn/go-sqlite3 for SQLite
+	container, err := sqlstore.New("sqlite3", "file:sessions.db?_foreign_keys=on", dbLog)
+	if err != nil {
+		panic(err)
+	}
+	// If you want multiple sessions, remember their JIDs and use .GetDevice(jid) or .GetAllDevices() instead.
+	deviceStore, err := container.GetFirstDevice()
+	if err != nil {
+		panic(err)
+	}
+	clientLog := waLog.Stdout("Client", "ERROR", true)
+	// Client
+	client := whatsmeow.NewClient(deviceStore, clientLog)
+	eventHandler := registerHandler(client)
+	client.AddEventHandler(eventHandler)
+
+	if client.Store.ID == nil {
+		// No ID stored, new login
+		qrChan, _ := client.GetQRChannel(context.Background())
+		err = client.Connect()
+		if err != nil {
+			panic(err)
+		}
+		for evt := range qrChan {
+			if evt.Event == "code" {
+				// Render the QR code here
+				qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
+				// or just manually `echo 2@... | qrencode -t ansiutf8` in a terminal
+				log.Println("Please Scan")
+			} else {
+				log.Println("Succes Login")
+			}
+		}
+	} else {
+		// Already logged in, just connect
+		err = client.Connect()
+		log.Println("Succes Login")
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// Listen to Ctrl+C (you can also do something else that prevents the program from exiting)
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+
+	client.Disconnect()
+}
+
+func init() {
+	ascii := figlet4go.NewAsciiRender()
+	renderStr, _ := ascii.Render("MYWA BOT")
+	// Set Browser
+	store.DeviceProps.PlatformType = waProto.DeviceProps_SAFARI.Enum()
+	store.DeviceProps.Os = proto.String("MYWA BOT")
+	// Print Banner
+	fmt.Print(renderStr)
+}
+
+func registerHandler(client *whatsmeow.Client) func(evt interface{}) {
+	return func(evt interface{}) {
+		switch v := evt.(type) {
+		case *events.Message:
+			go message.Msg(client, v)
+			break
+		}
+	}
+}
