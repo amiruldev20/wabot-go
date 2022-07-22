735d3262c0824f705519990a4af893cacc57f615
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2022-07-22 03:36:29 +0000

    Update main.go

--- main.go
+++ main.go
@@ -95,7 +95,7 @@ func init() {
 	}
 	renderStr, _ := ascii.Render(string(dxz))
 	// Set Browser
-	store.DeviceProps.PlatformType = waProto.DeviceProps_OHANA.Enum()
+	store.DeviceProps.PlatformType = waProto.DeviceProps_CHROME.Enum()
 	store.DeviceProps.Os = proto.String(string(dxz))
 	// Print Banner
 	fmt.Print(renderStr)
